e3eb220d8c79b5722a865015d4ab77081c61f679
Aleksa Milivojevic <iksmilivojevic@gmail.com>
2021-07-07 03:49:44 +0200

    DB i Context
    Iniciranje prazne Baze i dodavanje DBContext fajlova i neke sitnice

--- Back/WebApp/Startup.cs
+++ Back/WebApp/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Repository;
 
 namespace WebApp
 {
@@ -26,15 +28,21 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
+            services.AddDbContext<AuthenticationDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+            services.AddDbContext<DataDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DataConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AuthenticationDBContext db, DataDBContext dbase)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            db.Database.EnsureCreated();
+            dbase.Database.EnsureCreated();
 
             app.UseHttpsRedirection();
 
