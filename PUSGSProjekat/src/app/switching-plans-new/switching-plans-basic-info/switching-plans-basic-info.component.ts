32b1fbc4d81c4d2aa8bf2729d23e742e786f4aeb
LukaPantelic <lukapantelic1212@gmail.com>
2021-07-07 23:12:07 +0200

    Swirching-plans komponenta

--- PUSGSProjekat/src/app/switching-plans-new/switching-plans-basic-info/switching-plans-basic-info.component.ts
+++ PUSGSProjekat/src/app/switching-plans-new/switching-plans-basic-info/switching-plans-basic-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Incident } from 'src/app/models/incident..model';
+import { IncidentService } from 'src/app/services/incident-service/incident.service';
 
 @Component({
   selector: 'app-switching-plans-basic-info',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchingPlansBasicInfoComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+  public inc!: Incident
+
+  constructor(private service:IncidentService) { }
+
+
 
   ngOnInit(): void {
+    this.inc = this.service.currentIncident;
+    this.initForm();
+  }
+
+  private initForm() {
+    this.registerForm = new FormGroup({
+        'date': new FormControl(''),
+        'date1': new FormControl(''),
+        'voltage': new FormControl(''),
+        'description': new FormControl(''),
+        'date2': new FormControl(''),
+        'date3': new FormControl(''),
+        'date4': new FormControl(''),
+  
+    });
   }
 
 }
--- PUSGSProjekat/src/app/switching-plans-new/switching-plans-new.component.ts
+++ PUSGSProjekat/src/app/switching-plans-new/switching-plans-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Incident } from '../models/incident..model';
+import { IncidentService } from '../services/incident-service/incident.service';
 
 @Component({
   selector: 'app-switching-plans-new',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchingPlansNewComponent implements OnInit {
 
-  constructor() { }
+  incidents: Incident[] = new Array();
+
+  constructor(private service: IncidentService) {
+    let temp = new Incident;
+    temp.id = "WR1";
+    temp.incidentTime = new Date();
+    temp.status = "Draft";
+    temp.address = "TestAddr";
+    this.incidents.push(temp);
+    this.incidents.push(temp);
+    this.incidents.push(temp);
+    this.incidents.push(temp);
+    this.incidents.push(temp);
+    this.incidents.push(temp);
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.service.addIncident().subscribe(
+      (res:any)=>{
+
+      }
+    )
   }
 
 }
