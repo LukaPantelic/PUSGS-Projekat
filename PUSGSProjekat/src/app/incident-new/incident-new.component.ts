d9dee9d42429a8f6e3acd531d394412a58e0fbf0
LukaPantelic <lukapantelic1212@gmail.com>
2021-07-08 13:41:13 +0200

    Incident-newComponent,basicInformation, Incident-calls component

--- PUSGSProjekat/src/app/incident-new/incident-new.component.ts
+++ PUSGSProjekat/src/app/incident-new/incident-new.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Incident } from '../models/incident..model';
+import { IncidentService } from '../services/incident-service/incident.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-incident-new',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncidentNewComponent implements OnInit {
 
-  constructor() { }
+  incidents:Incident[] = new Array();
+
+  constructor(private service: IncidentService, private router:Router, private toastr: ToastrService) {
+      let temp = new Incident;
+      temp.id = "WR1";
+      temp.incidentTime = new Date();
+      temp.status = "Draft";
+      temp.address = "TestAddr";
+      this.incidents.push(temp);
+      this.incidents.push(temp);
+      this.incidents.push(temp);
+      this.incidents.push(temp);
+      this.incidents.push(temp);
+      this.incidents.push(temp);
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.service.addIncident().subscribe(
+      (res:any)=>{
+        this.router.navigateByUrl("/incident-browser");
+        this.toastr.success('You successfully added new incident!');
+      },
+      err=>{
+        console.log(err);
+        this.toastr.error('something went wrong');
+      }
+    )
   }
 
 }
