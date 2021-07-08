a0f1776c026735974344fa98bd8dcc473eb65450
LukaPantelic <lukapantelic1212@gmail.com>
2021-07-09 00:20:17 +0200

    addDeviceComponent, addDeviceNewComponent

--- PUSGSProjekat/src/app/add-device/add-device.component.ts
+++ PUSGSProjekat/src/app/add-device/add-device.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Device } from '../models/device.model';
+import { DeviceService } from '../services/device-service/device.service';
 
 @Component({
   selector: 'app-add-device',
   templateUrl: './add-device.component.html',
   styleUrls: ['./add-device.component.css']
 })
-export class AddDeviceComponent implements OnInit {
+export class AddDeviceComponent implements AfterViewInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'type', 'street'];
+  dataSource!: MatTableDataSource<Device>;
+  showAdd:boolean;
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private service: DeviceService) {
+      if(localStorage.Role!="Dispatcher")
+      {
+        this.showAdd = false;
+      }else {
+        this.showAdd = true;
+      }
+      this.dataSource = new MatTableDataSource();
+   }
+
+  ngAfterViewInit(): void {
+    this.service.getDevices().subscribe(
+      (res:any)=>{
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    )
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
--- PUSGSProjekat/src/app/add-new-device/add-new-device.component.ts
+++ PUSGSProjekat/src/app/add-new-device/add-new-device.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Device } from '../models/device.model';
+import { Street } from '../models/street.model';
+import { DeviceService } from '../services/device-service/device.service';
+import { StreetService } from '../services/street-service/street.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-new-device',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewDeviceComponent implements OnInit {
 
-  constructor() { }
+  selectedType!:string;
+  name!:string;
+  streets!:Street[];
+  selectedStreet!:string;
+
+  constructor(private streetService:StreetService, private deviceService:DeviceService,private toastr: ToastrService) { 
+    streetService.getStreets().subscribe(
+      (res:any)=>{
+        this.streets = res.retval;
+      },
+      err=>{
+        console.log(err);
+      }
+    )
+  }
 
   ngOnInit(): void {
+  }
+
+  onCauseChange($event:any){
+    this.refreshName();
+  }
+
+  onSubmit(){
+    var body:Device = new Device();
+    body.name = this.name;
+    body.street = this.selectedStreet;
+    body.type = this.selectedType;
+    this.deviceService.addDevice(body).subscribe(
+      (res:any)=>{
+        //toster pop up gj u did it
+        this.refreshName();
+        this.toastr.success('You added new device!');
+      },
+      err=>{
+        console.log(err);
+        this.toastr.error('Something went wrong');
+      }
+    )
+  }
+
+  refreshName(){
+    this.deviceService.getNextId(this.selectedType).subscribe(
+      (res:any)=>{
+        this.name = res.newId;
+        this.toastr.info('Name refreshed');
+      },
+      err=>{
+        console.log(err);
+      }
+    )
   }
 
 }
