ba04055eb3a5fbb215e1f651ba97015fadf31fc0
LukaPantelic <lukapantelic1212@gmail.com>
2021-07-09 13:41:16 +0200

    callsComponent

--- PUSGSProjekat/src/app/calls/calls.component.ts
+++ PUSGSProjekat/src/app/calls/calls.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Incident } from '../models/incident..model';
+import { IncidentService } from '../services/incident-service/incident.service';
+import { Call } from '../models/call.model';
+import { Street } from '../models/street.model';
+import { CallService } from '../services/call-service/call.service';
+import { StreetService } from '../services/street-service/street.service';
 
 @Component({
   selector: 'app-calls',
@@ -7,9 +14,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CallsComponent implements OnInit {
 
-  constructor() { }
+  streets!:Street[];
+  selectedStreet!:string;
+  hazard!:string;
+  comment!:string;
+  selectedReason!:string;
+
+  constructor(private callService:CallService, private streetService:StreetService) {
+    streetService.getStreets().subscribe(
+      (res:any)=>{
+        this.streets = res.retval;
+      },
+      err=>{
+        console.log(err);
+      }
+    )
+   }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    var body:Call = new Call();
+    body.comment = this.comment;
+    body.hazard = this.hazard;
+    body.reason = this.selectedReason;
+    body.street = this.selectedStreet;
+    this.callService.addCall(body).subscribe(
+      (res:any)=>{
+
+      },
+      err=>{
+        console.log(err);
+      }
+    );
+  }
 }
